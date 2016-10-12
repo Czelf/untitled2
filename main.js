77c75a5f760b0698b3ec19fa9f0ed8cff1118d65
arno cuyckens <r0372562@student.thomasmore.be>
2016-10-12 18:16:30 +0200

    einde oef min of meer

--- main.js
+++ main.js
@@ -3,13 +3,26 @@
  */
 var express =  require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.listen(4567);
+app.use(bodyParser.json());
 
-app.get("/book", function(request, response){
-    response.send ({name:"Beginning Node JS", rating:4})
+var books = [];
+books.push({id:1, name:"Beginning Node JS", rating:4});
+books.push({id:2, name:"Knoeien met data-distributie", rating:3});
+
+app.get("/books", function(request, response){
+    response.send (books)
 });
 
+app.post("/books", function(request, response){
+    var book = request.body;
+    book.id = books.length+1;
+    books.push(book);
+    response.status(204).send();
+});
+
+app.listen(4567);
 console.log("woow, dees werkt, ofni ofwel");
 
 /** http://localhost:4567/book */
